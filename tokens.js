2d5be7ec40962279bed1969cfe6ae0a2771c5ce4
Taras Bozhok <tbozhok.dev@gmail.com>
2026-07-31 10:19:36 +0300

    Import fix

--- tokens.js
+++ tokens.js
@@ -1,8 +1,6 @@
-var LLAMA3 = Object.freeze({
+export var LLAMA3 = Object.freeze({
     EOT: '<|eot_id|>',
     BOT: '<|begin_of_text|>',
     SHI: '<|start_header_id|>',
     EHI: '<|end_header_id|>',
 });
-
-export default Object.assign({}, LLAMA3);
